7092922c30dda9e2f739b9297620178c9c0c9810
sangTran <sangthien2010@gmail.com>
2020-04-15 12:20:48 -0500

    adding fib solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,32 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+function fibFast() {
+  let cache = {};
+  return function fib(n) {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      if (n < 2) {
+        return n;
+      } else {
+        cache[n] = fib(n - 1) + fib(n - 2);
+        return cache[n];
+      }
+    }
+  };
+}
+
+const fibFaster = fibFast();
+console.log("fast fib", fibFaster(40));
+console.log("slow fib", fib(40));
 
 module.exports = fib;
